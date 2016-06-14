c0acc5dcca108599aa3cb7fbc29c447e246a2651
Mike McDaid <mikemcdaid@live.com>
2016-06-14 15:15:08 +0100

    Added Zurb Foundation

---
+++ sites/all/themes/getonupband/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function getonupband_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function getonupband_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function getonupband_preprocess_node(&$variables) {
+}
