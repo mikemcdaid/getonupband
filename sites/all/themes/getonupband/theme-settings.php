97b34e1563ad6cd3b4ae69596e939a34ce4d6e3d
Mike McDaid <mikemcdaid@live.com>
2016-06-16 10:09:34 +0100

    Remvoved Foundation and its sub theme and added new Zen base and child themes

--- sites/all/themes/getonupband/theme-settings.php
+++ sites/all/themes/getonupband/theme-settings.php
@@ -1,7 +1,33 @@
 <?php
 /**
- * Implements hook_form_FORM_ID_alter().
+ * @file
+ * Contains the theme's settings form.
  */
-function getonupband_form_system_theme_settings_alter(&$form, &$form_state) {
 
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function getonupband_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
+  // Work-around for a core bug affecting admin themes. See issue #943212.
+  if (isset($form_id)) {
+    return;
+  }
+
+  // Create the form using Forms API: http://api.drupal.org/api/7
+
+  /* -- Delete this line if you want to use this setting
+  $form['getonupband_example'] = array(
+  '#type'          => 'checkbox',
+  '#title'         => t('getonupband sample setting'),
+  '#default_value' => theme_get_setting('getonupband_example'),
+  '#description'   => t("This example option doesn't do anything."),
+  );
+  // */
+
+  /* -- Delete this line if you want to remove this base theme setting.
+  // We don't need breadcrumbs to be configurable on this site.
+  unset($form['breadcrumb']);
+  // */
+
+  // We are editing the $form in place, so we don't need to return anything.
 }
