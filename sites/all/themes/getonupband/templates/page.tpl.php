a4cb725d11787a0ca272b88bf44e0893b2e6c6b3
Mike McDaid <mikemcdaid@live.com>
2016-06-28 13:25:28 +0100

    style updates

--- sites/all/themes/getonupband/templates/page.tpl.php
+++ sites/all/themes/getonupband/templates/page.tpl.php
@@ -92,32 +92,7 @@
       <?php print $feed_icons; ?>
     </main>
 
-    <div class="layout-swap__top layout-3col__full">
 
-      <a href="#skip-link" class="visually-hidden visually-hidden--focusable" id="main-menu" tabindex="-1">Back to top</a>
-
-      <?php if ($main_menu): ?>
-        <nav class="main-menu" role="navigation">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('navbar', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('visually-hidden'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-    </div>
 
     <?php if ($sidebar_first): ?>
       <aside class="<?php print $sidebar_first_class; ?>" role="complementary">
@@ -132,8 +107,13 @@
     <?php endif; ?>
   </div>
 
-  <?php print render($page['footer']); ?>
 
 </div>
 
-<?php print render($page['bottom']); ?>
+<div class="footer-bg">
+  <div class="layout-center">
+    <?php print render($page['footer']); ?>
+    <?php print render($page['bottom']); ?>
+  </div>
+</div>
+
